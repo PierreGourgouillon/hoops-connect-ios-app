870b0fec4e35546ab6c24eb0a95fa0b665a74c1c
Pierre <Pierre_gourgouillon@hotmail.com>
2023-06-29 12:00:26 +0200

    Initialize project

---
+++ hoops-connect-app/hoops_connect_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  hoops_connect_appApp.swift
+//  hoops-connect-app
+//
+//  Created by Pierre Gourgouillon on 29/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct hoops_connect_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
